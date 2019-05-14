1b99dde4764d2ef6473bd1cfe292da3b4f5d3665
Mateusz <matstanczak@gmail.com>
2019-05-14 22:28:43 +0100

    getAllReviewed added to ThemePark class

--- src/main/java/ThemePark/ThemePark.java
+++ src/main/java/ThemePark/ThemePark.java
@@ -1,14 +1,32 @@
 package ThemePark;
 
+import Interface.IReviewed;
+
+import java.util.ArrayList;
+
 public class ThemePark {
 
     private String name;
+    private ArrayList<IReviewed> reviews;
 
     public ThemePark(String name) {
         this.name = name;
+        this.reviews = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public int reviewsCount(){
+        return reviews.size();
+    }
+
+    public void addReview(IReviewed review){
+        reviews.add(review);
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        return reviews;
     }
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,21 +1,41 @@
+import Attraction.Playground;
+import Attraction.Rollercoster;
 import ThemePark.ThemePark;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class ThemeParkTest {
 
     ThemePark themePark;
+    Playground playground;
+    Rollercoster rollercoster;
 
     @Before
     public void before(){
         themePark = new ThemePark("GhostPark");
+        playground = new Playground(8, "MegaFun");
+        rollercoster = new Rollercoster(8, "SuperSpeed");
     }
 
     @Test
     public void hasName(){
         assertEquals("GhostPark", themePark.getName());
     }
+
+    @Test
+    public void allReviewedStartEmpty(){
+        assertEquals(0, themePark.reviewsCount());
+    }
+
+    @Test
+    public void canAddReview(){
+        themePark.addReview(playground);
+        assertEquals(1, themePark.reviewsCount());
+    }
+
 
 }
