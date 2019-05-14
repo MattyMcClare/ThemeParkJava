877d0a242f2cdae354e12a373eb42ba1218e4268
Mateusz <matstanczak@gmail.com>
2019-05-14 21:01:10 +0100

    tobaccoStallTest added

--- src/main/java/Stall/TobaccoStall.java
+++ src/main/java/Stall/TobaccoStall.java
@@ -1,4 +1,15 @@
 package Stall;
 
-public class TobaccoStall {
+public class TobaccoStall extends Stall {
+
+    private int minAge;
+
+    public TobaccoStall(String name, int rating, String ownerName, String parkingSpot) {
+        super(name, rating, ownerName, parkingSpot);
+        this.minAge = 18;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
 }
---
+++ src/test/java/TobaccoStallTest.java
@@ -0,0 +1,41 @@
+import Stall.IceCreamStall;
+import Stall.TobaccoStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TobaccoStallTest {
+
+    TobaccoStall tobaccoStall;
+
+    @Before
+    public void before(){
+        tobaccoStall = new TobaccoStall("BestIceCreamEver", 9, "Stachu", "bestOne");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("BestIceCreamEver", tobaccoStall.getName());
+    }
+
+    @Test
+    public void hasRating(){
+        assertEquals(9, tobaccoStall.getRating());
+    }
+
+    @Test
+    public void hasOwnerName(){
+        assertEquals("Stachu", tobaccoStall.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals("bestOne", tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void hasMinAge(){
+        assertEquals(18, tobaccoStall.getMinAge());
+    }
+}
