640158351033635e4f6d3e2021d0c5dfc487b691
Mateusz <matstanczak@gmail.com>
2019-05-14 20:57:46 +0100

    candyFlossStallTest added

--- src/main/java/Stall/CandyFlossStall.java
+++ src/main/java/Stall/CandyFlossStall.java
@@ -1,4 +1,8 @@
 package Stall;
 
-public class CandyFlossStall {
+public class CandyFlossStall extends Stall {
+
+    public CandyFlossStall(String name, int rating, String ownerName, String parkingSpot) {
+        super(name, rating, ownerName, parkingSpot);
+    }
 }
---
+++ src/test/java/CandyFlossStallTest.java
@@ -0,0 +1,36 @@
+import Stall.CandyFlossStall;
+import Stall.IceCreamStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CandyFlossStallTest {
+
+    CandyFlossStall candyFlossStall;
+
+    @Before
+    public void before(){
+        candyFlossStall = new CandyFlossStall("BestIceCreamEver", 9, "Stachu", "bestOne");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("BestIceCreamEver", candyFlossStall.getName());
+    }
+
+    @Test
+    public void hasRating(){
+        assertEquals(9, candyFlossStall.getRating());
+    }
+
+    @Test
+    public void hasOwnerName(){
+        assertEquals("Stachu", candyFlossStall.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals("bestOne", candyFlossStall.getParkingSpot());
+    }
+}
